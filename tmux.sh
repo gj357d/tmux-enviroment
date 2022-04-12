3459a633f868fae69bc5696f374c446e5c80b116
oliverbedi <52370286+oliverbedi@users.noreply.github.com>
2022-04-12 10:59:46 +0200

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -4,15 +4,15 @@ if [ $? != 0 ]
 then
     tmux new-session -s main -n "Main" -d
 
-    tmux new-window -n "CHIA" -t main
+    tmux new-window -n "Second" -t main
     tmux split-window -h -t main:1
-    tmux send-keys -t main:1.0 '. ./chia-blockchain/activate' C-m
+    tmux send-keys -t main:1.0 
     tmux send-keys -t main:1.1 'htop' C-m
 
 
-    tmux new-window -n "XMRig" -t main
+    tmux new-window -n "Last" -t main
     tmux split-window -h -p 34
-	tmux send-keys -t main:2.0 './xmrig -o rx.unmineable.com:3333 -u MATIC:0x848c1f5A8c437edb78Be969F629E03846321a8ca.B580#66pa-za5r -p B580 --threads=3' C-m
+	tmux send-keys -t main:2.0
 	tmux send-keys -t main:2.1 'htop' C-m
 
     tmux select-window -t main:1
